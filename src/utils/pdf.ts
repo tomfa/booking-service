39e4e60ac7b3ce0d965326215cc4b3556b661798
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-03-13 13:42:34 +0100

    pdf: add dummy pdf generate util

---
+++ src/utils/pdf.ts
@@ -0,0 +1,26 @@
+import {Browser} from "puppeteer-core";
+
+const chromium = require('chrome-aws-lambda');
+
+export const convertHTMLtoPDF = async (html: string): Promise<Buffer> => {
+  let browser: Browser;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto('https://vg.no');
+    return await page.pdf({path: './test.pdf'});
+  } finally {
+    if (!!browser) {
+      await browser.close();
+    }
+  }
+};
