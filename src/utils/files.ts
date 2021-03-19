7e6853a3ff245a70315c92e67561940678ffba8a
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-03-19 11:59:26 +0100

    upload pdf files to S3

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,5 +1,26 @@
-type FileData = { url: string }
-export const storeFile = async (content: Buffer): Promise<FileData> => {
-  // TODO: Store content as something
-  return { url: 'https://vg.no' }
-}
+import * as AWS from 'aws-sdk';
+import { v4 } from 'uuid';
+
+type FileData = { url: string };
+
+const s3 = new AWS.S3();
+const generateFileName = (fileEnding = 'pdf') => `${v4()}.${fileEnding}`;
+
+export const storeFile = async (
+  content: Buffer,
+  mimeType = 'application/pdf',
+  acl: 'public-read' | 'private' = 'public-read'
+): Promise<FileData> => {
+  const fileName = generateFileName();
+  await s3
+    .putObject({
+      Bucket: 'pdfs.webutvikling.org',
+      Key: fileName,
+      Body: content,
+      ContentType: mimeType,
+      ACL: acl
+    })
+    .promise();
+  const url = `https://s3.eu-north-1.amazonaws.com/pdfs.webutvikling.org/${fileName}`
+  return { url };
+};
