2aa5bfaa85bbd0f9f47697901c9142d00ecfbcf8
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-03-19 12:55:29 +0100

    #4 Implement GET /generate/from_template

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import * as Express from 'express';
-import { generatePdfFromHtml } from './controllers/generate';
+import {
+  generatePdfFromHtml,
+  generatePdfFromTemplate,
+} from './controllers/generate';
 
 const router = Express.Router();
 
 router.get('/generate/from_html', generatePdfFromHtml);
+router.get('/generate/from_template', generatePdfFromTemplate);
 
 export default router;
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -6,10 +6,19 @@ type FileData = { url: string };
 const s3 = new AWS.S3();
 const generateFileName = (fileEnding = 'pdf') => `${v4()}.${fileEnding}`;
 
+export const retrieveTemplate = async ( templateName: string ): Promise<string> => {
+  const prefix = 'templates';
+  const object = await s3.getObject({
+    Bucket: 'pdfs.webutvikling.org',
+    Key: `${prefix}/${templateName}`,
+  }).promise()
+  return object.Body.toString('utf-8');
+}
+
 export const storeFile = async (
   content: Buffer,
   mimeType = 'application/pdf',
-  acl: 'public-read' | 'private' = 'public-read'
+  acl: 'public-read' | 'private' = 'public-read',
 ): Promise<FileData> => {
   const fileName = generateFileName();
   const prefix = 'files';
@@ -19,9 +28,29 @@ export const storeFile = async (
       Key: `${prefix}/${fileName}`,
       Body: content,
       ContentType: mimeType,
-      ACL: acl
+      ACL: acl,
     })
     .promise();
-  const url = `https://s3.eu-north-1.amazonaws.com/pdfs.webutvikling.org/${prefix}/${fileName}`
+  const url = `https://s3.eu-north-1.amazonaws.com/pdfs.webutvikling.org/${prefix}/${fileName}`;
+  return { url };
+};
+
+export const storeTemplate = async ({ content, templateName, mimeType = 'text/html', acl = 'public-read' }: {
+  content: Buffer,
+  templateName: string,
+  mimeType: string,
+  acl: 'public-read' | 'private',
+}) => {
+  const prefix = 'templates';
+  await s3
+    .putObject({
+      Bucket: 'pdfs.webutvikling.org',
+      Key: `${prefix}/${templateName}`,
+      Body: content,
+      ContentType: mimeType,
+      ACL: acl,
+    })
+    .promise();
+  const url = `https://s3.eu-north-1.amazonaws.com/pdfs.webutvikling.org/${prefix}/${templateName}`;
   return { url };
 };
