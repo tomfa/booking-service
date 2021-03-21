bb7f6096b649816ede3dd893b3178b6fea094ce6
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-03-21 22:34:48 +0100

    infrastructure: add cors rules to s3 bucket

--- infrastructure/bucket.tf
+++ infrastructure/bucket.tf
@@ -1,19 +1,30 @@
 variable "aws_region" {
   description = "e.g. eu-north-1, us-east-1"
+  default = "eu-north-1"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket. E.g. files.mywebsite.com"
 }
 
-module "file-storage" {
-  source = "git::https://github.com/tomfa/terraform.git//files"
-  bucket_name = var.bucket_name
-  aws_region = var.aws_region
-  acl = "private"
+provider "aws" {
+  region = var.aws_region
 }
 
-output "BUCKET_NAME" {
-  value = module.file-storage.BUCKET_NAME
-  description = "Use this to configure CI for automatic deployment"
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+output "BUCKET_ENDPOINT" {
+  value = aws_s3_bucket.bucket.bucket_regional_domain_name
+  description = "S3 bucket endpoints"
 }
