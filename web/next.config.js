4cb86155a28ba2c6aeb75d3bfd1f8b74216e1132
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-03-27 10:11:00 +0100

    web: fix deployment to github pages

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,20 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
 const { findLastCommitSha } = require('./gitBuildId');
 
-module.exports = {
+const PRODUCTION_DOMAIN_ROOT = '/pdf-generator-api';
+const DEVELOPMENT_DOMAIN_ROOT = undefined; // = root of domain
+
+module.exports = (phase) => ({
   distDir: 'build',
+
+  basePath:
+    phase === PHASE_DEVELOPMENT_SERVER
+      ? DEVELOPMENT_DOMAIN_ROOT
+      : PRODUCTION_DOMAIN_ROOT,
+
   generateBuildId: async () => {
     const commitSha = await findLastCommitSha();
     return commitSha.substr(0, 7);
   },
-};
+});
