5bb6f771e742c6720e227d779d958babaee4815b
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-03-30 09:53:15 +0200

    web, FileList: fix UI bug for LoadingFileItem

--- web/components/FileList/LoadingFileItem.tsx
+++ web/components/FileList/LoadingFileItem.tsx
@@ -1,11 +1,11 @@
 import { IsoToDisplayDateTime } from '../utils/date.utils';
 import { LoadingListItem } from './LoadingFileItem.styles';
-import { DateStamp } from './FileItem.styles';
+import { DateStamp, ListItemText } from './FileItem.styles';
 
 export const LoadingFileItem = () => {
   return (
     <LoadingListItem>
-      <span>Justaplaceholder.html</span>
+      <ListItemText>Justaplaceholder.html</ListItemText>
       <DateStamp>{IsoToDisplayDateTime('2025-13-08T01:15:13')}</DateStamp>
     </LoadingListItem>
   );
