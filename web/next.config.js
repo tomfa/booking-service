5c2e133c6813e9d7de3707f810d755c20a4a07d8
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-04-01 12:19:57 +0200

    ci: update web deployment job

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 const { findLastCommitSha } = require('./gitBuildId');
 
-const PRODUCTION_DOMAIN_ROOT = '/pdf-generator-api';
+const PRODUCTION_DOMAIN_ROOT = undefined;
 const DEVELOPMENT_DOMAIN_ROOT = undefined; // = root of domain
 
 module.exports = phase => ({
