8b10e6423c9aba6667f06210f8bdd3924004e3c5
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-04-04 20:01:54 +0200

    shared: fix bad JSON type

--- shared/src/types/json.ts
+++ shared/src/types/json.ts
@@ -1,4 +1,10 @@
-export type JSONValue = boolean | number | string | null | JSONValue[];
+export type JSONValue =
+  | boolean
+  | number
+  | string
+  | null
+  | JSONValue[]
+  | JSONObject;
 
 export type JSONObject = {
   [key: string]: JSONValue;
