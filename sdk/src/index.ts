6c1d9f5215453051335a8c38a0ac3a17823f8958
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-05-22 19:55:15 +0200

    sdk: extract sdk code to own package
    #5

---
+++ sdk/src/index.ts
@@ -0,0 +1,6 @@
+import * as errors from './errors';
+import BookingAPI from './BookingAPI.mock';
+import * as types from './BookingAPI.types';
+import * as utils from './utils';
+
+export { BookingAPI, errors, types, utils };
