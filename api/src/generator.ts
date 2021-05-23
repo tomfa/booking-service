e479bd0be66ed288649f3aa736a151ecc27a1494
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-05-23 14:59:31 +0200

    refactor(api): remove irrelevant code from previous project

--- api/src/generator.ts
+++
@@ -1,5 +0,0 @@
-import serverless from 'serverless-http';
-import App from './app';
-import generatorRoutes from './generatorRoutes';
-
-module.exports.handler = serverless(new App(generatorRoutes).app);
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,13 +1,8 @@
-import { Router } from 'express';
-import { FOLDER } from '@booking-service/shared';
+import { Router, Request, Response } from 'express';
 import { errorWrapper } from './utils/errorHandler';
 import { ControllerFunction } from './types';
-import {
-  deleteFiles,
-  getUploadURL,
-  listFiles,
-} from './endpoints/controller.helper';
 import { login } from './endpoints/auth/login';
+import { getUserOrThrow } from './utils/auth/request.utils';
 
 const router = Router();
 
@@ -18,16 +13,13 @@ const post = (url: string, fun: ControllerFunction) =>
 const del = (url: string, fun: ControllerFunction) =>
   router.delete(url, errorWrapper(fun));
 
-get(`/${FOLDER.templates}`, listFiles(FOLDER.templates));
-del(`/${FOLDER.templates}`, deleteFiles(FOLDER.templates));
-get(`/${FOLDER.templates}/upload_url`, getUploadURL(FOLDER.templates));
-
-get(`/${FOLDER.fonts}`, listFiles(FOLDER.fonts));
-del(`/${FOLDER.fonts}`, deleteFiles(FOLDER.fonts));
-get(`/${FOLDER.fonts}/upload_url`, getUploadURL(FOLDER.fonts));
-
-get(`/${FOLDER.files}`, listFiles(FOLDER.files));
-del(`/${FOLDER.files}`, deleteFiles(FOLDER.files));
+get(`/me`, async (req: Request, res: Response) => {
+  const owner = getUserOrThrow(req);
+  return res.json({ data: owner, message: 'OK' });
+});
+del(`/me`, async (req: Request, res: Response) => {
+  return res.json({ data: 'No :(', message: 'OK' });
+});
 
 post('/auth/login', login);
 
