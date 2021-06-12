2a6701129d653cbe03bb72c6ee196ec41a589093
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-06-12 22:22:09 +0200

    test(web): proof-of-concept graphql query

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,10 +2,20 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useAuth } from '../providers/AuthProvider';
 import { Layout } from '../components/Layout';
+import { useFindResourcesQuery } from '../graphql/generated/types';
+import { config } from '../config';
 
 export default function Home() {
   const { isLoggedIn } = useAuth();
   const router = useRouter();
+  const { status, data, error, isFetching } = useFindResourcesQuery({
+    endpoint: config.GRAPHQL_ENDPOINT,
+    fetchParams: {
+      headers: {
+        'x-api-key': config.GRAPHQL_API_KEY,
+      },
+    },
+  });
   useEffect(() => {
     if (!isLoggedIn) {
       router.push('/login');
