fa43ac5483582876bcf26a57bd953a4868087cad
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-06-14 13:16:48 +0200

    fix(backend): fix bad seed sql

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,13 +1,13 @@
 INSERT INTO CUSTOMER (id, name, email, issuer) VALUES ('kroloftet-free', 'Torbjørn Larssen', 'torbjorn@kroloftet.no', 'auth.kroloftet.no');
 INSERT INTO RESOURCE (id, "customerId", label, seats, schedule) VALUES ('kroloftet-møterom', 'kroloftet-free', 'Møterom', 4, '{
-  "mon": { "start": "08:00", "end": "20:00", "slotDuration": 60, "slotInterval": 30 },
-  "tue": { "start": "08:00", "end": "20:00", "slotDuration": 60, "slotInterval": 30 },
-  "wed": { "start": "08:00", "end": "20:00", "slotDuration": 60, "slotInterval": 30 },
-  "thu": { "start": "08:00", "end": "20:00", "slotDuration": 60, "slotInterval": 30 },
-  "fri": { "start": "08:00", "end": "20:00", "slotDuration": 60, "slotInterval": 30 },
-  "sat": { "start": "10:00", "end": "16:00", "slotDuration": 60, "slotInterval": 30 },
-  "sun": { "start": "10:00", "end": "16:00", "slotDuration": 60, "slotInterval": 30 },
-  "overriddenDates": {}
+  "mon": { "start": "08:00", "end": "20:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "tue": { "start": "08:00", "end": "20:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "wed": { "start": "08:00", "end": "20:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "thu": { "start": "08:00", "end": "20:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "fri": { "start": "08:00", "end": "20:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "sat": { "start": "10:00", "end": "16:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "sun": { "start": "10:00", "end": "16:00", "slotDurationMinutes": 60, "slotIntervalMinutes": 30 },
+  "overriddenDates": []
 }');
 INSERT INTO BOOKING (
   id,
