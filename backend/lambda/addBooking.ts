d3261ac91f7ad29e56244b0eb3615e8fa118c04f
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-06-19 10:30:34 +0200

    fix(backend): support addBooking without end time

--- backend/lambda/addBooking.ts
+++ backend/lambda/addBooking.ts
@@ -1,34 +1,52 @@
-import { AddBookingInput, Booking } from '../graphql/generated/types';
+import { AddBookingInput, Booking, Resource } from '../graphql/generated/types';
 import { getDB } from './db';
 import { getId } from './utils/input.mappers';
 import { fromDBBooking } from './utils/db.mappers';
-import { genericErrorResponse } from './utils/response';
 import { ErrorType } from './utils/types';
+import getResourceById from './getResourceById';
+import { BadRequestError, ErrorCode } from './utils/errors';
+
+const isResource = (
+  object: Resource | null | ErrorType
+): object is Resource => {
+  if (!object) {
+    return false;
+  }
+  return !!(object as Resource).id;
+};
+
+const getEndTime = (start: Date, resource: Resource): Date => {
+  const slotDuration = resource.schedule.mon.slotDurationMinutes;
+  return new Date(start.getTime() + slotDuration * 60 * 1000);
+};
 
 async function addBooking({
   start,
   end,
-  canceled,
   ...booking
-}: AddBookingInput): Promise<Booking | ErrorType> {
+}: AddBookingInput): Promise<Booking> {
   const db = await getDB();
-  try {
-    // TODO: Error handling
-    //  - what if id already exists
-    const dbBooking = await db.booking.create({
-      data: {
-        ...booking,
-        canceled: !!canceled,
-        id: getId(booking.id),
-        startTime: new Date(start),
-        endTime: new Date(end),
-      },
-    });
-    return fromDBBooking(dbBooking);
-  } catch (err) {
-    console.log('Postgres error: ', err);
-    return genericErrorResponse;
+  // TODO: what if id already exists
+  const resource = await getResourceById(booking.resourceId);
+  if (!resource) {
+    throw new BadRequestError(
+      `Can not create booking on unknown resource`,
+      ErrorCode.RESOURCE_DOES_NOT_EXIST
+    );
   }
+
+  const startTime = new Date(start);
+  const endTime = (end && new Date(end)) || getEndTime(startTime, resource);
+  const dbBooking = await db.booking.create({
+    data: {
+      ...booking,
+      canceled: false,
+      id: getId(booking.id),
+      startTime,
+      endTime,
+    },
+  });
+  return fromDBBooking(dbBooking);
 }
 
 export default addBooking;
