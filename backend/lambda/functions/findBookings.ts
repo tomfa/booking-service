68caabbf873601d279137f8744c19ede33b00475
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-06-20 18:10:26 +0200

    fix(api): fix findBookings filter

--- backend/lambda/functions/findBookings.ts
+++ backend/lambda/functions/findBookings.ts
@@ -1,22 +1,23 @@
 import { PrismaClient } from '@prisma/client/scripts/default-index';
 import { Booking, FindBookingInput } from '../../graphql/generated/types';
 import { fromDBBooking } from '../utils/db.mappers';
+import { fromGQLDate } from '../utils/date.utils';
 
 async function findBookings(
   db: PrismaClient,
   { resourceIds, from, to, includeCanceled, ...args }: FindBookingInput
 ): Promise<Booking[]> {
+  const where = {
+    resourceId: (resourceIds && { in: resourceIds }) || undefined,
+    startTime: from ? { gte: fromGQLDate(from) } : undefined,
+    endTime: to ? { lte: fromGQLDate(to) } : undefined,
+    canceled: !includeCanceled ? false : undefined,
+    ...args,
+  };
   const bookings = await db.booking.findMany({
-    where: {
-      resourceId: (resourceIds && { in: resourceIds }) || undefined,
-      startTime: from ? { gte: new Date(from) } : undefined,
-      endTime: to ? { lte: new Date(to) } : undefined,
-      canceled: !includeCanceled ? false : undefined,
-      ...args,
-    },
+    where,
     include: { resource: true },
   });
-  console.log('bookings', JSON.stringify(bookings));
   return bookings.map(fromDBBooking);
 }
 
