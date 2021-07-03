6c9d4df72b94e20394f2979fa63fbef4aaaedbf3
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-07-04 00:47:07 +0200

    build(api): add express to gcp

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
 
-export const helloWorld = functions
-  .region('europe-central2')
-  .https.onRequest((request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Tomas!');
-  });
+admin.initializeApp();
+
+const app = express();
+
+app.get('/helloWorld', (request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Tomas!');
+});
+
+export const api = functions.region('europe-central2').https.onRequest(app);
