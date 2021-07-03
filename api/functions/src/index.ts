c78e062f96e901e39f98d22eb7bf091efae088b9
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-07-04 00:53:39 +0200

    build(api): allow any origin

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,10 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
+import * as cors from 'cors';
 
 admin.initializeApp();
 
 const app = express();
+
+app.use(cors({ origin: true }));
 
 app.get('/helloWorld', (request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
